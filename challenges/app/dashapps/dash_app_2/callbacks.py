e594b0a63560927184cc54f060af099ab1ae32fd
Anurag Verma <anurag.verma@fnmathlogic.com>
2021-09-27 12:39:07 +0000

    GCP Academy dashboard

--- challenges/app/dashapps/dash_app_2/callbacks.py
+++ challenges/app/dashapps/dash_app_2/callbacks.py
@@ -8,4 +8,44 @@ from flask_login import current_user
 from flask import flash
 
 def register_callbacks(app):
-    pass
+    @app.callback([Output('tab-content-1', 'hidden'), Output('tab-content-2', 'hidden'), Output('tab-content-3', 'hidden'), Output('tab-content-4', 'hidden'), Output('tab-content-5', 'hidden'), Output('tab-content-6', 'hidden')],
+                  [Input('tabs', 'value')])
+    def render_content_1(tab):
+        print("render_content ",tab)
+        if tab == 'tab1':
+            return False, True, True, True, True, True
+        elif tab == 'tab2':
+            return True, False, True, True, True, True
+        elif tab == 'tab3':
+            return True, True, False, True, True, True
+        elif tab == 'tab4':
+            return True, True, True, False, True, True
+        elif tab == 'tab5':
+            return True, True, True, True, False, True
+        elif tab == 'tab6':
+            return True, True, True, True, True, False
+        else:
+            return False, True, True, True, True, True
+
+    @app.callback([Output('hidden-data','data')],
+        [Input('refresh','n_clicks')])
+    def get_base_data(n_clicks):
+        print("refreshing data")
+        try:
+            userid = current_user.email
+            #print(userid)
+            js = get_gcp_academy_data(userid)
+            return [js]
+        except Exception as e:
+            print("No User email or Error in accessing data")
+            print(e)
+
+    @app.callback([Output('df31','data')],
+        [Input('hidden-data','data')])
+    def update_df31(js):
+        print("updating df31")
+        import pandas as pd
+        df31 = pd.read_json(js)
+        df31 = df31[df31['userid'] == current_user.email]
+        return [df31.to_dict('records')]
+
--- challenges/app/dashapps/dash_app_2/layout.py
+++ challenges/app/dashapps/dash_app_2/layout.py
@@ -10,11 +10,29 @@ from app.dashapps.dash_app_2.base_styles import *
 from app.dashapps.dash_app_2.base_data import *
 import dash_pivottable
 #layout = html.Div("Placeholder for "+current_app.config['DASH_APP_2_MENU_TEXT'])
-layout = html.Div(children=[
-    html.Br(),
-    dash_table.DataTable(
-    id='overall_table',
-    css=dashtable_css,merge_duplicate_headers=True, style_header=style_header, style_data_conditional=style_data_conditional,
-    columns=columns_overall,
-    data=df_overall.to_dict('records'),
-)])
+layout = dbc.Col(html.Div(children=[html.Br(),
+html.Div(html.H4(id='dashboard-heading',style={'width':'100%','margin':'auto'})),
+html.Button("Refresh",id='refresh', n_clicks=0, className = 'btn btn-primary btn-lg', hidden=True),
+dcc.Store(id='hidden-data'),
+dcc.Tabs(id='tabs', value='tab3', children=[
+                dcc.Tab(label='Overall', id='tab1', value='tab1',disabled=True),
+                dcc.Tab(label='Active Events', id='tab2', value='tab2', disabled = True),
+                dcc.Tab(label='GCP Academy', id='tab3', value='tab3'),
+                dcc.Tab(label='DS Challenges', id='tab4', value='tab4', disabled = True),
+                dcc.Tab(label='AWS Academy', id='tab5', value='tab5', disabled = True),
+                dcc.Tab(label='All Submissions', id='tab6', value='tab6', disabled = True),
+                ]),
+                html.Div(id='tab-content-1',children=[
+                    dash_table.DataTable(id='df11',css=dashtable_css,columns=columns11,data=df11[col11].to_dict('records'),editable=True, row_selectable='multi', merge_duplicate_headers=True, sort_action = 'native' ,style_header=style_header, style_data_conditional=style_data_conditional)]),
+                html.Div(id='tab-content-2',children=[
+                    dash_table.DataTable(id='df21',css=dashtable_css,columns=columns21,data=df21[col21].to_dict('records'),editable=True, merge_duplicate_headers=True,  sort_action = 'native' ,style_header=style_header, style_data_conditional=style_data_conditional)]),
+                html.Div(id='tab-content-3',children=[
+                    dash_table.DataTable(id='df31',css=dashtable_css,columns=columns31, merge_duplicate_headers=True, style_header=style_header, sort_action = 'native',filter_action="native" , style_data_conditional=style_data_conditional)]),
+                html.Div(id='tab-content-4',children=[
+                    dash_table.DataTable(id='df41',css=dashtable_css,columns=columns41,data=df41[col41].to_dict('records'), merge_duplicate_headers=True, style_header=style_header, sort_action = 'native' , style_data_conditional=style_data_conditional)]),
+                html.Div(id='tab-content-5',children=[
+                    dash_table.DataTable(id='df51',css=dashtable_css,columns=columns51,data=df51[col51].to_dict('records'), merge_duplicate_headers=True, style_header=style_header, sort_action = 'native' , style_data_conditional=style_data_conditional)]),
+                html.Div(id='tab-content-6',children=[
+                    dash_table.DataTable(id='df61',css=dashtable_css,columns=columns61,data=df61[col61].to_dict('records'), merge_duplicate_headers=True, style_header=style_header, sort_action = 'native' , style_data_conditional=style_data_conditional)]),
+                    ]))
+
